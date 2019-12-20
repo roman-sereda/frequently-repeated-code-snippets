8ec5442f7862fe8d1ae40b78c456391452522d40
Roman Sereda <roman.sereda@pm.me>
2019-12-20 17:07:52 +0200

    Add react component snippet

---
+++ react/component.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Name extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+
+  }
+
+  render(){
+    return();
+  }
+}
+
+Name.defaultProps = {
+
+};
+
+export default Name;
